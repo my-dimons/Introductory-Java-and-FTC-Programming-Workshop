c3bc76fabccb0d47a9cf22103bb1098eb12a8c1a
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2024-11-07 15:20:00 -0800

    Added Week 2 Material

---
+++ WorkshopMaterial/Week 2/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        // String (Stores text, such as "Hello". String values are surrounded by double quotes)
+        String stringName = "Hanson";
+        System.out.println(stringName);
+
+        // Int (Stores integers (whole numbers), from -2,147,483,648 to 2,147,483,647)
+        int intName = 2;
+
+        // Float (Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits)
+        float floatName = 2.99f;
+
+        // Double (Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits)
+        double doubleName = 5.531;
+
+        // Char (Stores a single character/letter or ASCII values)
+        char charName = 'D';
+
+        // Boolean (Stores true or false values)
+        boolean boolName = true;
+    }
+}
